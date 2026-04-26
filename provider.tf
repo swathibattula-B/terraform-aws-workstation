8d4e0973d6ae825b403cb4fda2791d5922f7d760
swathi <battulaswathi15407@gmail.com>
2026-04-26 14:25:28 +0530

    terraform

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.33.0" # Terraform AWS provider version
+    }
+  }
+  backend "s3" {
+    bucket = "mylife-myrule1" 
+    key    = "mylife-state.tfstate"
+    region = "us-east-1"
+    encrypt = true
+    use_lockfile = true
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
